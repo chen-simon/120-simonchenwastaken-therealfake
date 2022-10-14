05c8d0e29aab5222a27b713cc670c3a3c21e4a7c
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-10-13 23:35:41 -0400

    fix cart items

--- src/components/CartItemBox/CartItemBox.js
+++ src/components/CartItemBox/CartItemBox.js
@@ -1,13 +1,23 @@
 import './CartItemBox.scss';
 
 const CartItemBox = (props) => {
+  const incBtn = document.querySelectorAll(".increase");
+  const decBtn = document.querySelectorAll(".decrease");
+
+  incBtn.forEach(btn => {
+    btn.onclick = () => console.log('add!')
+  })
+
+  decBtn.forEach = (btn => {
+
+  })
 
   return (
     <div class="cartItemBox">
       <img src={props.item.image} alt={props.item.name} />
-      <div class="item-quantity">
+      <div class="item-quantity-box">
         <button class="decrease">-</button>
-        <div>0</div>
+        <div class="item-quantity">0</div>
         <button class="increase">+</button>
       </div>
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,11 @@ import CartItemBox from '../CartItemBox/CartItemBox';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const Footer = (props) => {
-
     return (
       <div id="footer">
         <div>Your Cart</div>
         <div id="cart-items">
-          { props.cart && props.cart.map(item => <CartItemBox item={ item }/>) }
+          { props.cart && props.cart[0].items.map(item => <CartItemBox item={ item }/>) }
         </div>
         <div id="totals">
           <div>Subtotal: </div>
