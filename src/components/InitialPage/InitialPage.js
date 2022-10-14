27886c9a1f158330a7caf60b75603af98690c684
Simon Chen <simonchen.sc.2002@gmail.com>
2022-10-14 01:58:50 -0400

    🌎 Styling changes

--- src/components/InitialPage/InitialPage.js
+++ src/components/InitialPage/InitialPage.js
@@ -3,8 +3,8 @@ import './InitialPage.scss';
 const InitialPage = (props) => {
 
   return (
-    <div id="inital-page">
-      Please Sign In To Start Ordering
+    <div id="initial-page">
+      🍜 Please Sign In To Start Ordering 🍚
     </div>
 
   );
