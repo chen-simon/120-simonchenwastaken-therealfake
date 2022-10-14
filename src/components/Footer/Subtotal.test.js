e7323589467a7097f3045366616a2ffef1ed75a9
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-10-14 14:31:09 -0400

    add jest test

--- src/components/Footer/Subtotal.test.js
+++ src/components/Footer/Subtotal.test.js
@@ -1,7 +1,10 @@
-const getSubtotal = require('./Subtotal.js');
+import '@testing-library/jest-dom';
+import getSubtotal from './Subtotal';
 
-test('returns the subtotal of an array of items', () => {
-    const item1 = {name: "1", price: 1, quantity: 2}
-    const item2 = {name: "1", price: 1, quantity: 2}
-    expect(getSubtotal([item1, item2])).toBe(4);
-})
+describe('subtotal function', () => {
+    test('returns the subtotal of an array of items', () => {
+        const item1 = {name: "1", price: 1, quantity: 2}
+        const item2 = {name: "1", price: 1, quantity: 2}
+        expect(getSubtotal([item1, item2])).toBe(4);
+    });
+}) ;
